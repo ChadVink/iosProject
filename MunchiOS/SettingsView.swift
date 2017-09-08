805e4a548c91ab04b6e7022e0176844569136245
Eli Simmonds <eli@carmelsoft.com>
2017-09-07 18:01:22 -0700

    added controls and picker options to settings

--- MunchiOS/SettingsView.swift
+++ MunchiOS/SettingsView.swift
@@ -9,15 +9,44 @@
 import Foundation
 import UIKit
 
-class SettingsView: UIViewController {
+class SettingsView: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
+    @IBOutlet weak var distanceSlider: UISlider!
+    @IBOutlet weak var priceControl: UISegmentedControl!
+    @IBOutlet weak var typePicker: UIPickerView!
+    
+    var pickerDataSource = ["Lunch", "Dinner", "Breakfast", "Bars", "Happy Hour"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Connect data:
+        self.typePicker.delegate = self
+        self.typePicker.dataSource = self
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerDataSource.count;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerDataSource[row]
+    }
+    
+    @IBAction func distanceChange(_ sender: Any) {
+        print(distanceSlider.value)
+    }
+    @IBAction func priceControlChange(_ sender: Any) {
+        print(priceControl.selectedSegmentIndex)
+    }
+    
 }
